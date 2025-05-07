e8202d71c1378b10e8e09833fd3528fa270bef76
Fileurix3 <145297541+Fileurix3@users.noreply.github.com>
2025-05-07 15:40:59 +0300

    update nvim config

---
+++ config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,11 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      names = true,        -- поддержка color names (red, blue)
+      css = true,          -- включить в CSS
+      tailwind = true,     -- поддержка Tailwind классов (по желанию)
+      mode = "background", -- можно "foreground" или "virtualtext"
+    },
+  },
+}
